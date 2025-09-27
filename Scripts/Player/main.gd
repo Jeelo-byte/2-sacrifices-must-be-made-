44c2d48b29da2ad0fc4e1080c88f8e00edec0a6e
“jeelo-byte” <jeel.gajera.edu@gmail.com>
2025-09-27 15:48:21 -0500

    init

---
+++ Scripts/Player/main.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+# You can delete the old code from this file.
+func _ready():
+	pass
+
+func _process(delta):
+	pass
---
+++ Scripts/Player/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@export var bullet: PackedScene
+@export var bullet_speed = 1000
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+var movespeed = 500
+
+# --- NEW ---
+# Moved the animation logic here from main.gd.
+func _ready():
+	anim.play("idle")
+
+func _physics_process(delta):
+	var direction = Vector2()
+	
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	
+	if Input.is_action_just_pressed("LMB"):
+		fire()
+	
+	if direction != Vector2.ZERO:
+		velocity = direction.normalized() * movespeed
+	else:
+		velocity = Vector2.ZERO 
+		
+	move_and_slide()
+	look_at(get_global_mouse_position())
+	
+func fire():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.global_position = global_position
+	bullet_instance.rotation = rotation
+	bullet_instance.apply_impulse(Vector2(bullet_speed, 0).rotated(rotation))
+	get_tree().get_root().add_child(bullet_instance)
