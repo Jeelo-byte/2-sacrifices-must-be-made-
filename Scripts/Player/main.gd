ca3cc18b2491dcb489320ff7a208a4260c8ebfad
Piyush Mallick <piyushm1729@gmail.com>
2025-09-27 20:07:19 -0500

    Spawn areas has been made circular around the player, collisions and enemy count added

--- Scripts/Player/main.gd
+++ Scripts/Player/main.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 @export var enemy_scene: PackedScene
-@export var enemy_count: int = 5
-@export var spawn_area: Rect2 = Rect2(Vector2.ZERO, Vector2(1024, 768))
+@export var enemy_count: int = 99
+@export var spawn_distance_min: float = 200
+@export var spawn_distance_max: float = 500
 
 func _ready():
 	spawn_enemies()
 
 func spawn_enemies():
+	var player = $Player
+	
 	for i in range(enemy_count):
 		var e = enemy_scene.instantiate()
-		var x = randi() % int(spawn_area.size.x) + spawn_area.position.x
-		var y = randi() % int(spawn_area.size.y) + spawn_area.position.y
-		e.position = Vector2(x, y)
+		var angle = randf() * TAU
+		var distance = randf_range(spawn_distance_min, spawn_distance_max)
+		
+		var spawn_pos = player.position + Vector2(cos(angle), sin(angle)) * distance
+		
+		e.position = spawn_pos
 		add_child(e)
--- Sences/enemy.gd
+++ Sences/enemy.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 @onready var player = get_parent().get_node("Player")
+var move_speed = 500
+
+func _ready():
+	add_to_group("enemies")
 
 func _physics_process(delta):
-	position += (player.position - position) / 50
+	var direction = (player.position - position).normalized()
+	velocity = direction * move_speed
+	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("bullets") or body.name.begins_with("Bullet"):
