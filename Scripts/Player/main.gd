1ac0e61c649af3953aa7f866690155d723fd2a5e
Piyush Mallick <piyushm1729@gmail.com>
2025-09-27 19:02:50 -0500

    Added spawning for multiple enemies.

--- Scripts/Player/main.gd
+++ Scripts/Player/main.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
-# You can delete the old code from this file.
-func _ready():
-	pass
+@export var enemy_scene: PackedScene
+@export var enemy_count: int = 5
+@export var spawn_area: Rect2 = Rect2(Vector2.ZERO, Vector2(1024, 768))
 
-func _process(delta):
-	pass
+func _ready():
+	spawn_enemies()
+
+func spawn_enemies():
+	for i in range(enemy_count):
+		var e = enemy_scene.instantiate()
+		var x = randi() % int(spawn_area.size.x) + spawn_area.position.x
+		var y = randi() % int(spawn_area.size.y) + spawn_area.position.y
+		e.position = Vector2(x, y)
+		add_child(e)
