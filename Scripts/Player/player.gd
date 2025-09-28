b4e0d0751e13e4b8ffe9928f31a622782f2dab87
Piyush Mallick <piyushm1729@gmail.com>
2025-09-28 06:48:14 -0500

    Machine gun mode implemented.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -3,23 +3,33 @@ extends CharacterBody2D
 @export var bullet_speed = 1500
 @export var offset_scale = 140
 @export var offset_rot = 270
-@export var max_health: int = 1
+@export var max_health: int = 100
 @export var damage_cooldown: float = 1.0
 @onready var anim: AnimatedSprite2D = $PlayerAnim
 signal health_changed(current_health: int, max_health: int)
+
 var movespeed = 500
 var current_health: int
 var can_take_damage: bool = true
+
 var dash_drain_timer: float = 0.0
 var dash_drain_rate: float = 1.0
+
+var machine_gun_timer: float = 0.0
+var machine_gun_rate: float = 0.1
+var machine_gun_drain_timer: float = 0.0
+var machine_gun_drain_rate: float = 0.5
+
 func _ready():
 	add_to_group("player")
 	anim.play("idle")
 	current_health = max_health
 	call_deferred("emit_initial_health")
 	global_position = Vector2(0, 0)
+
 func emit_initial_health():
 	health_changed.emit(current_health, max_health)
+
 func _physics_process(delta):
 	var direction = Vector2()
 	if current_health != 0:
@@ -34,9 +44,21 @@ func _physics_process(delta):
 		
 		if Input.is_action_just_pressed("LMB"):
 			fire()
-		
+		elif Input.is_action_pressed("LMB") and Input.is_action_pressed("machine_gun"):
+			machine_gun_timer += delta
+			machine_gun_drain_timer += delta
+			if machine_gun_timer >= machine_gun_rate:
+				fire()
+				machine_gun_timer = 0.0
+			if machine_gun_drain_timer >= machine_gun_drain_rate:
+				current_health = max(current_health - 1, 0)
+				health_changed.emit(current_health, max_health)
+				machine_gun_drain_timer = 0.0
+		else:
+			machine_gun_timer = 0.0
+			machine_gun_drain_timer = 0.0
+			
 		var is_dashing = Input.is_action_pressed("dash")
-		
 		if direction != Vector2.ZERO:
 			var speed = movespeed
 			if is_dashing:
@@ -56,12 +78,14 @@ func _physics_process(delta):
 		
 		move_and_slide()
 		look_at((get_global_mouse_position() - anim.global_position).rotated(deg_to_rad(offset_rot)) + anim.global_position)
+
 func fire():
 	var bullet_instance = bullet.instantiate()
 	bullet_instance.global_position = anim.global_position + offset_scale * Vector2(cos(rotation + deg_to_rad(offset_rot + 180)), sin(rotation + deg_to_rad(offset_rot + 180)))
 	bullet_instance.rotation = rotation + deg_to_rad(offset_rot)
 	bullet_instance.apply_impulse(Vector2(bullet_speed, 0).rotated(rotation + deg_to_rad(offset_rot + 180)))
 	get_tree().get_root().add_child(bullet_instance)
+
 func take_damage(damage_amount: int):
 	if not can_take_damage:
 		return
@@ -70,11 +94,13 @@ func take_damage(damage_amount: int):
 	create_damage_effect()
 	can_take_damage = false
 	get_tree().create_timer(damage_cooldown).timeout.connect(func(): can_take_damage = true)
+
 func create_damage_effect():
 	var tween = create_tween()
 	tween.set_loops(3)
 	tween.tween_property(anim, "modulate", Color.RED, 0.1)
 	tween.tween_property(anim, "modulate", Color.WHITE, 0.1)
+
 func die():
 	current_health = 0
 	var game_over_screen = get_node("../UI/GameOverScreen")
